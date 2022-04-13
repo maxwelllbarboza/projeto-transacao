56f41fd8408cfed38cd37524459ec2d32a8576d0
maxwellbarboza@yahoo.com <maxwellbarboza@yahoo.com>
2022-04-13 01:37:52 -0300

    Deletando arquivos não usados

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import 'styles.css';
+
+function Example (){
+  return(
+    <h1>Header</h1>
+  );
+}
+
+export default Example;
---
+++ src/pages/Main/index.js
@@ -0,0 +1,11 @@
+import './styles.css';
+
+function Main() {
+  return (
+    <div className="App">
+      <h1>App</h1>
+    </div>
+  );
+}
+
+export default Main;
